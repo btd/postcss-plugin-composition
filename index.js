e825c033efd6cff02ae89dafeaae4477fbec55e0
Denis Bardadym <bardadymchik@gmail.com>
2016-10-19 15:20:35 +0300

    Fix typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-plugin-composition', function(plugins) {
-  if(Array.isArray(plugins)) {
+  if(!Array.isArray(plugins)) {
     throw new Error('`options` for postcss-plugin-composition must be array of plugins');
   }
 
